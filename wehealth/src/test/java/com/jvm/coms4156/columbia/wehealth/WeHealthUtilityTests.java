74223fb76f40e52e5aec92bc3e6ffcb7335a1b09
Derek Jin <sotskin@gmail.com>
2020-11-16 23:40:40 -0500

    unit test for utility class

---
+++ wehealth/src/test/java/com/jvm/coms4156/columbia/wehealth/WeHealthUtilityTests.java
@@ -0,0 +1,81 @@
+package com.jvm.coms4156.columbia.wehealth;
+
+import com.jvm.coms4156.columbia.wehealth.dto.ExerciseHistoryDetailsDto;
+import com.jvm.coms4156.columbia.wehealth.dto.ExerciseHistoryResponseDto;
+import com.jvm.coms4156.columbia.wehealth.dto.ExerciseRecordDto;
+import com.jvm.coms4156.columbia.wehealth.dto.UserIdDto;
+import com.jvm.coms4156.columbia.wehealth.entity.DBUser;
+import com.jvm.coms4156.columbia.wehealth.entity.ExerciseHistory;
+import com.jvm.coms4156.columbia.wehealth.entity.ExerciseType;
+import com.jvm.coms4156.columbia.wehealth.exception.BadRequestException;
+import com.jvm.coms4156.columbia.wehealth.exception.MissingDataException;
+import com.jvm.coms4156.columbia.wehealth.exception.NotFoundException;
+import com.jvm.coms4156.columbia.wehealth.repository.DBUserRepository;
+import com.jvm.coms4156.columbia.wehealth.repository.ExerciseHistoryRepository;
+import com.jvm.coms4156.columbia.wehealth.repository.ExerciseTypeRepository;
+import com.jvm.coms4156.columbia.wehealth.service.ExerciseService;
+import javassist.Loader;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.jvm.coms4156.columbia.wehealth.utility.Utility;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import static com.jvm.coms4156.columbia.wehealth.common.Constants.*;
+
+@SpringBootTest
+public class WeHealthUtilityTests {
+    @Test
+    public void getStringFromDateTest() {
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            String testDateString = "2020-11-16 11:11:11";
+            Date testDate = df.parse(testDateString);
+            Assertions.assertEquals(true,
+                    testDateString.equals(Utility.getStringFromDate(testDate)));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /* TODO(Derek Jin): using Date() to get current time makes it difficult to test
+            Consider changing implementation to use java.time.Clock
+     */
+    @Test
+    public void getStringOfCurrentDateTimeTest() {
+        Utility.getStringOfCurrentDateTime();
+    }
+
+    @Test
+    public void getStringOfStartDateTimeInvalidUnit() {
+        Assertions.assertThrows(BadRequestException.class, () -> {
+           Utility.getStringOfStartDateTime("BadTestUnit", 1);
+        });
+    }
+
+    @Test
+    public void getStringOfStartDateTimeYear() {
+        Utility.getStringOfStartDateTime(YEAR, 1);
+    }
+
+    @Test
+    public void getStringOfStartDateTimeWeek() {
+        Utility.getStringOfStartDateTime(WEEK, 1);
+    }
+
+    @Test
+    public void getStringOfStartDateTimeMonth() {
+        Utility.getStringOfStartDateTime(MONTH, 1);
+    }
+
+}
