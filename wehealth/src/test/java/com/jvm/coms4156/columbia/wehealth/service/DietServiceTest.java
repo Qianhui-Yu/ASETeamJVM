6cdae098282d85da447723f92b58d7bf040068fb
Qianhui-Yu <qianhuiyu96@163.com>
2020-11-17 11:50:59 -0500

    Create DietServiceTest.java

---
+++ wehealth/src/test/java/com/jvm/coms4156/columbia/wehealth/service/DietServiceTest.java
@@ -0,0 +1,11 @@
+package com.jvm.coms4156.columbia.wehealth.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DietServiceTest {
+  
+}
