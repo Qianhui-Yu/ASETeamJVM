7dc521ca95c1184c9fc48fc262d95cc36853bd32
Sotskin <sotskin@gmail.com>
2020-12-02 01:52:11 -0500

    frontend working

--- wehealth/src/main/java/com/jvm/coms4156/columbia/wehealth/controller/AdviceController.java
+++ wehealth/src/main/java/com/jvm/coms4156/columbia/wehealth/controller/AdviceController.java
@@ -8,10 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @Log4j2
 public class AdviceController extends BaseController {
@@ -27,9 +29,8 @@ public class AdviceController extends BaseController {
    */
   @GetMapping(path = "/advice", consumes = MediaType.APPLICATION_JSON_VALUE,
       produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<AdviceDto> getDietRecords(
-      @RequestBody UserIdDto userIdDto) {
-    log.info("Get adivce {}", userIdDto.toString());
+  public ResponseEntity<AdviceDto> getDietRecords() {
+    log.info("Get advice {}", au().toString());
     AdviceDto adviceDto = adviceService.getAdvice(au());
     return new ResponseEntity<>(adviceDto, HttpStatus.OK);
   }
