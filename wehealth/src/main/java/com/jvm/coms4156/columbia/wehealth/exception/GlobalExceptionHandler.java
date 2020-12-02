cf9609c3e255864f38c5c07bb5a2cdb0d9c1841a
Qianhui-Yu <qianhuiyu96@163.com>
2020-12-01 23:53:24 -0500

    Update GlobalExceptionHandler.java

--- wehealth/src/main/java/com/jvm/coms4156/columbia/wehealth/exception/GlobalExceptionHandler.java
+++ wehealth/src/main/java/com/jvm/coms4156/columbia/wehealth/exception/GlobalExceptionHandler.java
@@ -23,4 +23,35 @@ public class GlobalExceptionHandler {
   public ResponseEntity<ExceptionResponse> handleBadRequestException(Exception e) {
     return new ResponseEntity<>(new ExceptionResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
   }
+
+  @ExceptionHandler(MissingDataException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ResponseEntity<ExceptionResponse> handleMissingDataException(Exception e) {
+    return new ResponseEntity<>(new ExceptionResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(NotFoundException.class)
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  public ResponseEntity<ExceptionResponse> handleNotFoundException(Exception e) {
+    return new ResponseEntity<>(new ExceptionResponse(e.getMessage()), HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(PermissionException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ResponseEntity<ExceptionResponse> handlePermissionException(Exception e) {
+    return new ResponseEntity<>(new ExceptionResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(BadAuthException.class)
+  @ResponseStatus(HttpStatus.UNAUTHORIZED)
+  public ResponseEntity<ExceptionResponse> handleBadAuthException(Exception e) {
+    return new ResponseEntity<>(new ExceptionResponse("Authentication failed"), HttpStatus.UNAUTHORIZED);
+  }
+
+  @ExceptionHandler(DuplicateException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ResponseEntity<ExceptionResponse> handleDuplicateException(Exception e) {
+    return new ResponseEntity<>(new ExceptionResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
+  }
+
 }
